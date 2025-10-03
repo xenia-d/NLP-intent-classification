4c263f7142c526f21d2f249edf526f98cc69d182
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-10-03 21:18:46 +0200

    fixed train-test split

--- WildGuard_Dataset/load_dataset.py
+++ WildGuard_Dataset/load_dataset.py
@@ -4,10 +4,15 @@ import pandas as pd
 def load_dataset():
 
     df_train = pd.read_parquet("hf://datasets/allenai/wildguardmix/train/wildguard_train.parquet")
-    df_test = pd.read_parquet("hf://datasets/allenai/wildguardmix/test/wildguard_test.parquet")
+    # df_test = pd.read_parquet("hf://datasets/allenai/wildguardmix/test/wildguard_test.parquet")
 
     # For quicker testing, a subset of the training data 
     df_train = df_train.iloc[:10000].reset_index(drop=True)
+
+    # divide df_train into train and test sets (80-20 split)
+    df_test = df_train.sample(frac=0.2, random_state=12)
+    df_train = df_train.drop(df_test.index).reset_index(drop=True)
+    df_test = df_test.reset_index(drop=True)
 
     df_train = df_train.dropna(subset=["prompt_harm_label"])
     df_test = df_test.dropna(subset=["prompt_harm_label"])
