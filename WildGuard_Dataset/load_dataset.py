881e69afe1442d3682498cdf93f40e673040ffc0
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-10-03 12:08:25 +0200

    dataloaders

--- WildGuard_Dataset/load_dataset.py
+++ WildGuard_Dataset/load_dataset.py
@@ -6,13 +6,13 @@ def load_dataset():
     df_train = pd.read_parquet("hf://datasets/allenai/wildguardmix/train/wildguard_train.parquet")
     df_test = pd.read_parquet("hf://datasets/allenai/wildguardmix/test/wildguard_test.parquet")
 
+    # For quicker testing, a subset of the training data 
+    df_train = df_train.iloc[:10000].reset_index(drop=True)
+
+    df_train = df_train.dropna(subset=["prompt_harm_label"])
+    df_test = df_test.dropna(subset=["prompt_harm_label"])
+
+    print(f"Train dataset size: {len(df_train)}")
+    print(f"Test dataset size: {len(df_test)}")
+
     return df_train, df_test
-
-
-
-# print(f"Train size: {len(df_train)}")
-# print(f"Test size: {len(df_test)}")
-
-# print(df_train.columns)
-
-# print(df_test.columns)
--- train.py
+++ train.py
@@ -1,11 +1,25 @@
 import torch
 from torch.utils.data import DataLoader
-from datasets import load_dataset
 from WildGuard_Dataset.WildGuardMixDataset import WildGuardMixDataset
 from WildGuard_Dataset.load_dataset import load_dataset
 
 
 df_train, df_test = load_dataset()
 
-print(f"Train size: {len(df_train)}")
-print(f"Test size: {len(df_test)}")
+train_dataset = WildGuardMixDataset(df_train)
+test_dataset = WildGuardMixDataset(df_test)
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=4)
+
+print(" Training batch example")
+for i, batch in enumerate(train_loader):
+    print(batch)
+    if i >= 1:  # just first 2 batches
+        break
+
+print(" Test batch example ")
+for i, batch in enumerate(test_loader):
+    print(batch)
+    if i >= 1:
+        break
