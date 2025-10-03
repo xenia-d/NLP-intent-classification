238d5a0499b623efdb1a8961408cdd52bbb7e2c0
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-10-03 11:09:10 +0200

    remove response columns

--- WildGuard_Dataset/load_dataset.py
+++ WildGuard_Dataset/load_dataset.py
@@ -7,3 +7,10 @@ df_test = pd.read_parquet("hf://datasets/allenai/wildguardmix/test/wildguard_tes
 print(f"Train size: {len(df_train)}")
 print(f"Test size: {len(df_test)}")
 
+print(df_train.columns)
+
+# remove response-relevant columns and keep only the prompt ones
+df_train = df_train.drop(columns=['response', 'response_refusal_label', 'response_harm_label'])
+df_test = df_test.drop(columns=['response', 'response_refusal_label', 'response_harm_label'])
+
+print(df_train.columns)
