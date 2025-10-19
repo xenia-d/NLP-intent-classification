53575435b3d5fa3535959ee2e16eeef281ada834
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-10-19 15:59:16 +0200

    added annotations 6 (complete annotations)

--- LLM_baseline.py
+++ LLM_baseline.py
@@ -71,7 +71,7 @@ def main():
     print("CUDA version:", torch.version.cuda)
     print("GPU:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "None")
 
-    annotations_path = "Annotations/annotations_5.csv"
+    annotations_path = "Annotations/annotations_6.csv"
     prompt_template_path = "Annotations/annotation_guidelines_prompt.txt"
 
     # Load annotations and and prompt template
